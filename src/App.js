0e4e23869edc370f5e33a939cacd1d19f65d71a3
Halina Tulchii <halendeihalyna@gmail.com>
2023-09-25 16:46:33 +0300

    updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='todo__wrapper'>
+
+        <div className="todo__box-neon">
+          <span className='todo__box-neon-title'> Create your better future by achieving small goals</span>
+          <span className='todo__box-neon-gradient'></span>
+          <span className='todo__box-neon-dodge'></span>
+        </div>
+        
+        <div className='todo__box-input'>
+        <input
+          type='text'
+          className='todo__input'
+          placeholder='Write your goal for today'
+        />
+        <button className='todo__btn'>
+              Add goal
+        </button>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
